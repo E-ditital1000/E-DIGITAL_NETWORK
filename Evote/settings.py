8d8bf37b34fe0491be43200cf188e97d1c20f727
E-digital1000 <techelvis7@gmail.com>
2023-05-19 23:27:31 +0000

    first

--- Evote/settings.py
+++ Evote/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%5pjt-7je2m7qf8g7&-ssjqa!1ih+160(8^fw5w!0tgb!8a5nl
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['your_username.pythonanywhere.com']
+ALLOWED_HOSTS = ['tech03.pythonanywhere.com']
 
 
 # Application definition
