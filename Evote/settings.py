dabbbf1eb05738cfa03a61a17003347c4f32758d
E-ditital1000 <techelvis7@gmail.com>
2024-06-23 03:03:32 -0700

    unify

--- Evote/settings.py
+++ Evote/settings.py
@@ -48,12 +48,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'vote',
     'crispy_forms',
     'crispy_bootstrap5',
-    'cloudinary',
-    'cloudinary_storage',
+    
     
 ]
 
